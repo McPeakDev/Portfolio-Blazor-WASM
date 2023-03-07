7a8bc852d98374248158de6742a52c43ac42b683
Matthew L. McPeak <m.mcpeakcs@gmail.com>
2023-03-07 15:45:36 -0500

    Initial commit

---
+++ Models/Project.cs
@@ -0,0 +1,24 @@
+namespace portfolio.Models;
+
+public class Owner {
+    public string? avatar_url { get; set; }
+
+}
+
+public class Project {
+    
+    public int Id { get; set; } 
+
+    public Owner? owner { get; set; }
+    
+    public string? name { get; set; } 
+
+    public string? language { get; set; } 
+
+    public DateTime updated_at { get; set; } 
+
+    public string? description { get; set; }
+
+    public string? html_url { get; set; }
+
+}
